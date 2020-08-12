e14d818ec9ce517659d16359c2f105c514858242
Will Lehman <william.p.lehman@usace.army.mil>
2020-08-12 14:07:01 -0700

    Adding @material-ui/core and @material-ui/icons for a single icon...

--- aep-map/src/containers/main/banner.js
+++ aep-map/src/containers/main/banner.js
@@ -2,25 +2,11 @@ import React from 'react';
 import { connect } from 'redux-bundler-react';
 import fema from '../../resources/FEMA_logo-white.png';
 import usace from '../../resources/USACE_logo.png';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 class Banner extends React.Component{
-    constructor(props){
-        super(props);
-        this.renderMsg = this.renderMsg.bind(this);
-    }
-    renderMsg(){
-        const {authIsLoggedIn, authUsername} = this.props;
-        if(authIsLoggedIn){
-          return (
-            <span>{ authUsername }</span>
-          )
-        }else{
-            return <span>Sign in</span>
-        }
-        
-      }
     render(){
-        const { doAuthLogin, authIsLoggedIn} = this.props;
+        let { doAuthLogin, authIsLoggedIn} = this.props;
         return (
             <nav className="navbar navbar=expand-lg navbar-dark bg-dark">
                 <div className="float-left">
@@ -30,7 +16,13 @@ class Banner extends React.Component{
                     <span style={{width: "10px"}}>      </span>
                     <a className="navbar-brand" href="/">AEP Map</a>            
                 </div>
-                <button className="btn btn-secondary mr-2" onClick={ doAuthLogin } disabled ={ authIsLoggedIn} >{this.renderMsg()}</button>
+                <button className="btn btn-secondary mr-2" onClick={ doAuthLogin } disabled ={ authIsLoggedIn} >
+                    {(authIsLoggedIn)?
+                        ("My Account"):
+                        ("Sign In")
+                    }
+                    <AccountCircleIcon style={{fontSize:30,paddingLeft:"5px"}}/>
+                </button>
             </nav>
         )
     }
