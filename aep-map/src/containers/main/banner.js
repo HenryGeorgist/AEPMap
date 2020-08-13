ab38cb2795c3dfc9893417bf72ee7ee8e9b8fa85
Will Lehman <william.p.lehman@usace.army.mil>
2020-08-12 17:59:36 -0700

    Fixes #6 - stole images from model library, im not proud, im resourceful

--- aep-map/src/containers/main/banner.js
+++ aep-map/src/containers/main/banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'redux-bundler-react';
-import fema from '../../resources/FEMA_logo-white.png';
-import usace from '../../resources/USACE_logo.png';
+import fema from '../../resources/FEMA_logo.png';
+import usace from '../../resources/usace3.png';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 class Banner extends React.Component{
@@ -10,8 +10,8 @@ class Banner extends React.Component{
         return (
             <nav className="navbar navbar=expand-lg navbar-dark bg-dark">
                 <div className="float-left">
-                    <a href = "/"><img src={usace} alt="USACE LOGO" style={{width:'80px'}}/></a>
-                    <a href = "/"><img src={fema} alt="FEMA LOGO" style={{width:'80px', paddingLeft:"5px"}}/></a>
+                    <a href = "/"><img src={usace} alt="USACE LOGO" style={{width:'45px'}}/></a>
+                    <a href = "/"><img src={fema} alt="FEMA LOGO" style={{width:'45px', paddingLeft:"5px"}}/></a>
                     <a className="navbar-brand" href="/" style={{paddingLeft:"15px"}}><b>AEP Map</b></a>            
                 </div>
                 <button className="btn btn-secondary mr-2" onClick={ doAuthLogin } disabled ={ authIsLoggedIn} >
